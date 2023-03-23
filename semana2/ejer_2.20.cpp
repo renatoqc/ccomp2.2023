1eb0f76f84064fa3626e5b27257a84f60950366c
Renato <renato.quispe@ucsp.edu.pe>
2023-03-23 12:04:25 -0500

    ejercicios casa

--- semana2/ejer_2.20.cpp
+++ semana2/ejer_2.20.cpp
@@ -4,11 +4,22 @@ using namespace std;
 int main ()
 {
     int radio, diametro, circunferencia, area;
-    int pi=3.14;
+    int pi=3.14159;
+
+    cout<<"Ingrese un numero: ";
+    cin>>radio;
+
     radio = 2*pi;
     diametro = radio*2;
-    circunferencia
+    circunferencia = pi*diametro;
+    area = pi*(radio*radio);
 
-
+    cout<<"El radio es: "<<radio<<endl;
+    cout<<"El diametro es: "<<diametro<<endl;
+    cout<<"La circunferencia es: "<<circunferencia<<endl;
+    cout<<"El area es: "<<area<<endl;
+    
+    
+    return 0;
 
 }
